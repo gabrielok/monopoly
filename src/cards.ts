983bd43aaae0dc4de96804ee393469a8491fc7b5
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-01-04 15:13:32 -0300

    refactor: move, name and document all anonymous functions in cards.ts

--- src/cards.ts
+++ src/cards.ts
@@ -1,17 +1,35 @@
 import { groupBy } from "@umatch/utils/array";
 
-import { arrestPlayer, movePlayer } from "./actions";
+import { arrestPlayer, movePlayer, transferMoney } from "./actions";
 
 import type Game from "./game";
 import type { Property } from "./interfaces/property";
 import type { Site } from "./interfaces/site";
 import type Player from "./player";
 
+type ParametersExceptFirstAndSecond<T extends (...args: any) => unknown> = T extends (
+  arg0: any,
+  arg1: any,
+  ...rest: infer R
+) => unknown
+  ? R
+  : never;
+
 export type Card = {
   action: (player: Player, game: Game) => void;
   description: string;
 };
 type CardInitializer = [string, (player: Player, game: Game) => void];
+
+/**
+ * Returns a function, which increases the number of Get Out of Jail Free cards
+ * the player has.
+ */
+function acquireGetOutOfJailFreeCard() {
+  return (player: Player) => {
+    player.getOutOfJailFreeCards += 1;
+  };
+}
 
 /**
  * Returns a function, which advances the player to the nearest property
@@ -39,6 +57,16 @@ function alterBalance(value: number) {
 }
 
 /**
+ * Returns a function, which, for each player in the game, transfers money
+ * between the calling player and the other player.
+ */
+function transferMoneyToEachPlayer(value: number) {
+  return (player1: Player, game: Game) => {
+    game.players.forEach((player2) => transferMoney(player1, player2, value));
+  };
+}
+
+/**
  * Returns a function, which taxes a player based on how many houses
  * and hotels it has on all its sites.
  */
@@ -55,81 +83,65 @@ function collectTaxes(houseTax: number, hotelTax: number) {
   };
 }
 
+/**
+ * A wrapper around movePlayer to reduce verbosity.
+ */
+function move(...args: ParametersExceptFirstAndSecond<typeof movePlayer>) {
+  const [stepsOrDestination, collect] = args;
+  return (player: Player, game: Game) =>
+    movePlayer(player, game, stepsOrDestination, collect);
+}
+
+/**
+ * Returns a function, which directly alters the player's position, without
+ * looping around the board or any other special behavior.
+ */
+function moveSimple(steps: number) {
+  return (player: Player) => {
+    player.position -= steps;
+  };
+}
+
 const _chanceCards: CardInitializer[] = [
-  [
-    "Advance to Go. Collect 2M.",
-    (player: Player, game: Game) => movePlayer(player, game, "Go", true),
-  ],
-  [
-    "Advance to Istambul. If you pass Go, collect 2M.",
-    (player: Player, game: Game) => movePlayer(player, game, "Istambul"),
-  ],
-  [
-    "Advance to London. If you pass Go, collect 2M.",
-    (player: Player, game: Game) => movePlayer(player, game, "London", true),
-  ],
-  [
-    "Advance to Montreal.",
-    (player: Player, game: Game) => movePlayer(player, game, "Montreal", false),
-  ],
+  ["Advance to Go. Collect 2M.", move("Go", true)],
+  ["Advance to Istambul. If you pass Go, collect 2M.", move("Istambul")],
+  ["Advance to London. If you pass Go, collect 2M.", move("London", true)],
+  ["Advance to Montreal.", move("Montreal", false)],
   [
     "Advance to the Monopoly Rail Transport. If you pass Go, collect 2M.",
-    (player: Player, game: Game) => movePlayer(player, game, "Monopoly Rail", true),
+    move("Monopoly Rail", true),
   ],
   [
-    "Advance to the nearest Transport. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled. If you pass Go, collect 2M.",
+    "Advance to the nearest Transport. If unowned, you may buy it from the Bank. " +
+      "If owned, pay owner twice the rental to which they are otherwise entitled. " +
+      "If you pass Go, collect 2M.",
     advanceToNearest("transport"),
   ],
   [
-    "Advance to the nearest Transport. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled. If you pass Go, collect 2M.",
+    "Advance to the nearest Transport. If unowned, you may buy it from the Bank. " +
+      "If owned, pay owner twice the rental to which they are otherwise entitled. " +
+      "If you pass Go, collect 2M.",
     advanceToNearest("transport"),
   ],
   [
-    "Advance to the nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total 10,000 times the amount thrown. If you pass Go, collect 2M.",
+    "Advance to the nearest Utility. If unowned, you may buy it from the Bank. " +
+      "If owned, throw dice and pay owner a total 10,000 times the amount thrown. " +
+      "If you pass Go, collect 2M.",
     advanceToNearest("utility"),
   ],
   ["Collect 500K.", alterBalance(500)],
   ["Collect 1.5M.", alterBalance(1500)],
   ["For each house pay 250K, for each hotel pay 1M.", collectTaxes(250, 1000)],
-  [
-    "Get out of Jail free.",
-    (player: Player) => {
-      player.getOutOfJailFreeCards += 1;
-    },
-  ],
-  [
-    "Go back 3 spaces.",
-    (player: Player) => {
-      player.position -= 3;
-    },
-  ],
+  ["Get out of Jail free.", acquireGetOutOfJailFreeCard],
+  ["Go back 3 spaces.", moveSimple(-3)],
   ["Go directly to Jail. Do not pass Go, do not collect 2M.", arrestPlayer],
   ["Pay 150K.", alterBalance(-150)],
-  [
-    "Pay each player 500K.",
-    (player: Player, game: Game) => {
-      game.players.forEach((other) => {
-        player.balance -= 500;
-        other.balance += 500;
-      });
-    },
-  ],
+  ["Pay each player 500K.", transferMoneyToEachPlayer(500)],
 ];
 
 const _chestCards: CardInitializer[] = [
-  [
-    "Advance to Go. Collect 2M.",
-    (player: Player, game: Game) => movePlayer(player, game, "Go", true),
-  ],
-  [
-    "Collect 100K from each player.",
-    (player: Player, game: Game) => {
-      game.players.forEach((other) => {
-        player.balance += 100;
-        other.balance -= 100;
-      });
-    },
-  ],
+  ["Advance to Go. Collect 2M.", move("Go", true)],
+  ["Collect 100K from each player.", transferMoneyToEachPlayer(-100)],
   ["Collect 100K.", alterBalance(100)],
   ["Collect 100K.", alterBalance(100)],
   ["Collect 200K.", alterBalance(200)],
@@ -139,12 +151,7 @@ const _chestCards: CardInitializer[] = [
   ["Collect 1M.", alterBalance(1000)],
   ["Collect 2M.", alterBalance(2000)],
   ["For each house pay 400K, for each hotel pay 1.15M.", collectTaxes(400, 1150)],
-  [
-    "Get out of Jail free.",
-    (player: Player) => {
-      player.getOutOfJailFreeCards += 1;
-    },
-  ],
+  ["Get out of Jail free.", acquireGetOutOfJailFreeCard],
   ["Go directly to Jail. Do not pass Go, do not collect 2M.", arrestPlayer],
   ["Pay 500K.", alterBalance(-500)],
   ["Pay 1M.", alterBalance(-1000)],
