4f7c1d047c46872c3f6897cc7383e2da491e6625
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-01-11 21:30:23 -0300

    refactor: wrap the code in an async function

--- test/simulate.ts
+++ test/simulate.ts
@@ -12,35 +12,47 @@ import { PROPERTIES } from "../src/properties";
 
 const MAX_ITER = 100_000;
 
-const visited = {} as { [_ in Place]: number };
-function registerVisit(player: Player, game: Game) {
-  const place = game.board[player.position];
-  visited[place] = (visited[place] ?? 0) + 1;
-}
+async function simulate() {
+  const visited = {} as { [_ in Place]: number };
+  function registerVisit(player: Player, game: Game) {
+    const place = game.board[player.position];
+    visited[place] = (visited[place] ?? 0) + 1;
+  }
 
-const player = new Player("James");
-const game = new Game(
-  [player],
-  BOARD,
-  PROPERTIES,
-  shuffle(chanceCards),
-  shuffle(chestCards),
-);
-let lastPos;
-console.log(chalk.green(`Collecting data from ${MAX_ITER} iterations...`));
-for (let i = 0; i < MAX_ITER; i += 1) {
-  const steps = randomInteger(1, 6) + randomInteger(1, 6);
-  movePlayer(player, game, steps, true);
-  lastPos = player.position;
-  registerVisit(player, game);
-  processPlace(player, game);
-  // the player may move multiple times per turn due to chance cards
-  while (lastPos !== player.position) {
+  const player = new Player("James");
+  const game = new Game(
+    [player],
+    BOARD,
+    PROPERTIES,
+    shuffle(chanceCards),
+    shuffle(chestCards),
+  );
+  let lastPos;
+  console.log(chalk.green(`Collecting data from ${MAX_ITER} iterations...`));
+  for (let i = 0; i < MAX_ITER; i += 1) {
+    const steps = randomInteger(1, 6) + randomInteger(1, 6);
+    movePlayer(player, game, steps, true);
+    lastPos = player.position;
     registerVisit(player, game);
     processPlace(player, game);
-    lastPos = player.position;
+    // the player may move multiple times per turn due to chance cards
+    while (lastPos !== player.position) {
+      registerVisit(player, game);
+      processPlace(player, game);
+      lastPos = player.position;
+    }
   }
+
+  const formatted = apply(visited, (count) => round((count / MAX_ITER) * 100, 2));
+  console.log(stringify(formatted, { pad: true }));
 }
 
-const formatted = apply(visited, (count) => round((count / MAX_ITER) * 100, 2));
-console.log(stringify(formatted, { pad: true }));
+(async () => {
+  try {
+    await simulate();
+  } catch (error) {
+    console.debug(error);
+  }
+  console.log(chalk.whiteBright.bgRed("End simulation"));
+  process.exit();
+})();
